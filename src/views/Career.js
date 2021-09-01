335fb7a8ae8b79119be66dd6e653ddbe9960d80e
Phil Arfuso <arfusop@gmail.com>
2021-09-01 18:04:22 -0400

    Started building out Career section

--- src/views/Career.js
+++ src/views/Career.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Timeline, Popover } from 'antd'
+
+import styles from './views.module.scss'
 
 const Career = () => {
-    return <div>Careers go here</div>
+    return (
+        <section className={styles.Career}>
+            <h3>Career</h3>
+            <Timeline>
+                <Popover
+                    placement="topLeft"
+                    content={`Things I did here`}
+                    title="MarketView Research (March 2014 - February 2018)">
+                    <Timeline.Item>First Job</Timeline.Item>
+                </Popover>
+                <Timeline.Item>Second Job</Timeline.Item>
+                <Timeline.Item>MEMX</Timeline.Item>
+            </Timeline>
+        </section>
+    )
 }
 
 export default Career
