58a8cd0b3cadea2c85f840e92aa3086868991a99
Phil Arfuso <arfusop@gmail.com>
2021-12-05 23:10:42 -0500

    added theme switcher

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,8 @@ export const ABOUT = 'ABOUT'
 export const SKILLS = 'SKILLS'
 export const PROJECTS = 'PROJECTS'
 export const CAREER = 'CAREER'
+
+// theMe related vars
+export const LOCAL_STORAGE_THEME_KEY = 'parfuso.me_theme'
+export const DARK = 'DARK'
+export const LIGHT = 'LIGHT'
