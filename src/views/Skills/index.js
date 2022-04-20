aa18c7c61c73c97b6c4c15aae5e2c5de45a22806
Phil Arfuso <arfusop@gmail.com>
2022-04-20 09:19:30 -0400

    fix up styling of skills

--- src/views/Skills/index.js
+++ src/views/Skills/index.js
@@ -15,37 +15,57 @@ const Skills = () => {
             className={styles.Skills}>
             <h1>Skills & Abilities</h1>
             <div className={styles.skillSector}>
-                <h2>Front End Tools & Frameworks</h2>
-                <hr />
-                <h3>JavaScript | TypeScript</h3>
-                <ul>
-                    <li>ES6 and beyond</li>
-                </ul>
-                <h3>JS Frameworks</h3>
-                <ul>
-                    <li>React</li>
-                    <li>Next.js</li>
-                </ul>
-                <h3>HTML | CSS</h3>
-                <ul>
-                    <li>SASS</li>
-                    <li>CSS Modules</li>
-                    <li>Styled Components/CSS in JS</li>
-                </ul>
-                <h2>Back End Tools & Frameworks</h2>
-                <hr />
-                <h3>Node | TypeScript</h3>
-                <ul>
-                    <li>Express</li>
-                    <li>Restful APIs</li>
-                    <li>GraphQL APIs</li>
-                    <li>Apollo & Prisma</li>
-                </ul>
-                <h3>Databases</h3>
-                <ul>
-                    <li>MongoDB</li>
-                    <li>PostGres (SQL Based)</li>
-                </ul>
+                <div>
+                    <h2>Front End Tools & Frameworks</h2>
+                    <p>
+                        Extremely comfortable with all of the skillsets listed
+                        here except for TypeScript. Actively learning and trying
+                        to master that language.
+                    </p>
+                </div>
+                <div className={styles.list}>
+                    <h3>JavaScript | TypeScript</h3>
+                    <ul>
+                        <li>ES6 and beyond</li>
+                    </ul>
+                    <h3>JS Frameworks</h3>
+                    <ul>
+                        <li>React</li>
+                        <li>Next.js</li>
+                    </ul>
+                    <h3>HTML | CSS</h3>
+                    <ul>
+                        <li>SASS</li>
+                        <li>CSS Modules</li>
+                        <li>Styled Components/CSS in JS</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={styles.skillSector}>
+                <div>
+                    <h2>Back End Tools & Frameworks</h2>
+                    <p>
+                        I have production level experience working in PostGres &
+                        with Restful APIs. My experience with GraphQL & MongoDB
+                        is more exploratory and used in side projects.
+                    </p>
+                </div>
+                <div className={styles.list}>
+                    <h3>Node | TypeScript</h3>
+                    <ul>
+                        <li>Express</li>
+                        <li>Restful APIs</li>
+                        <li>GraphQL APIs</li>
+                        <li>Apollo & Prisma</li>
+                    </ul>
+                    <div>
+                        <h3>Databases</h3>
+                        <ul>
+                            <li>MongoDB</li>
+                            <li>PostGres (SQL Based)</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </section>
     )
